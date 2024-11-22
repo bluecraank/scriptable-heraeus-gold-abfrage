23ce127e07e12e0281eaae1ebf52d7a3d6c4e6c8
Nils Fischer <bluecraank@outlook.de>
2024-11-22 15:37:29 +0100

    Update

--- index.js
+++ index.js
@@ -1,11 +1,3 @@
-// This is a scriptable app that will be run by the Scriptable app on your iOS device.
-
-let loginUrl = "https://www.heraeus-gold.de/de/login"
-let baseUrl = "https://www.heraeus-gold.de/de/mein_golddepot/meinsparplan/step1"
-let username = "yourUsername"
-let password = "yourPassword"
-
-
 let request = new Request(loginUrl)
 request.method = "POST"
 request.addParameterToMultipart("username", username)
@@ -29,16 +21,6 @@ let response2 = await request2.loadString()
 
 const wv = new WebView();
 await wv.loadHTML(response2);
-const body = await wv.evaluateJavaScript(`document.body.innerHTML`);
-console.log(body)
-
-// console.log(response2)
-
-
 // // Now get content of div class sp_statBody
-// let regex = /<div class="sp_statBody">(.*)<\/div>/
-// let match = response2.match(regex)
-
-// if (match) {
-//     console.log(match[1])
-// }
+const content = await wv.evaluateJavaScript(`document.querySelector('.sp_statBody').innerHTML`)
+console.log(content)
